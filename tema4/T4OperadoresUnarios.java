366f0f20e0d9902c825bf44e2a3d93bfbfac5fb3
Javier Caselli <7252806+javiercaselli@users.noreply.github.com>
2024-10-18 17:14:12 +0200

    Ejemplos operadores unarios y setencia switch

---
+++ tema4/T4OperadoresUnarios.java
@@ -0,0 +1,31 @@
+package tema4;
+
+/**
+ * Clase para demostrar el uso de los operadores unarios
+ */
+public class T4OperadoresUnarios {
+    public static void main(String[] args) {
+        // Comentario de una sola línea
+        /*
+         * Este comentario se puede extender
+         * a lo largo de varias líneas
+         * 
+         * Se utiliza para aclaraciones del código más extensas
+         */
+
+        // Prueba de operadores unarios
+        int numero = 100;
+
+        /*
+         * Operador incremento sufijo (numero++)
+         */
+        int incrementoSufijo = numero--;
+        System.out.println(incrementoSufijo);
+
+        /*
+         * Operador incremento prefijo (++numero)
+         */
+        int incrementoPrefijo = --numero;
+        System.out.println(incrementoPrefijo);
+    }
+}
