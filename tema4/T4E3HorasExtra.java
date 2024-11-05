9cbfc6fe7138eb11a8a59b234b01533660ac5ae3
Javier Caselli <7252806+javiercaselli@users.noreply.github.com>
2024-11-05 17:09:59 +0100

    T4 Ejercicio 3

--- tema4/T4E3HorasExtra.java
+++ tema4/T4E3HorasExtra.java
@@ -8,17 +8,26 @@ import java.util.Scanner;
 public class T4E3HorasExtra {
     public static void main(String[] args) {
         // Declaraciones
-        final float PRECIO_HORA = 12f;
-        float horas;
+        final int HORAS_LABORABLES = 40;
+        final float PRECIO_HORA = 12.0f;
+        final float PRECIO_EXTRA = 16.0f;
+        int horas;
         float sueldo;
         
         // Lee las horas trabajadas 
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduzca el número de horas trabajadas durante la semana: ");
-        horas = sc.nextFloat();
+        horas = sc.nextInt();
 
         // Calcula el sueldo semanal
-        sueldo = horas * PRECIO_HORA;
+        if (horas <= HORAS_LABORABLES) {
+            sueldo = horas * PRECIO_HORA;
+        } else {
+            // Primeras 40 horas
+            sueldo = HORAS_LABORABLES * PRECIO_HORA;
+            int horasExtra = horas - HORAS_LABORABLES;
+            sueldo += horasExtra * PRECIO_EXTRA;
+        }
 
         // Muestra el resultado por pantalla
         System.out.printf("El sueldo semanal que le corresponde es de %.2f euros.\n", sueldo);
