a0af8989e7dd4873b9fb42c27d13e7516861518b
Javier Caselli <7252806+javiercaselli@users.noreply.github.com>
2024-12-17 16:57:04 +0100

    Tema 7 - Ejemplos

---
+++ tema7Arrays/PruebaRellenoRandom.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class PruebaRellenoRandom {
+    public static void main(String[] args) {
+        int[] lista = new int[500];
+        Random rand = new Random();
+
+        int[] array9 = new int[9];
+        String[][] tresEnRaya = new String[3][3];
+
+        tresEnRaya[0][0] = "O";
+        tresEnRaya[2][2] = "X";
+        
+        for(int i=0; i<lista.length; i++) {
+            lista[i] = rand.nextInt();
+        }
+
+        for(int i=0; i<lista.length; i++) {
+            System.out.println(lista[i]);
+        }
+
+    }
+}
